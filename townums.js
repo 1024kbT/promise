1ce0fe927af941a340ac4e3ee5749ba49dcb279b
Your Name <you@example.com>
2021-10-13 13:05:05 +0800

    towsun

--- townums.js
+++ townums.js
@@ -4,15 +4,21 @@
  * @Autor: lwq
  * @Date: 2021-09-29 17:25:24
  * @LastEditors: Seven
- * @LastEditTime: 2021-09-30 09:26:16
+ * @LastEditTime: 2021-10-13 13:05:00
  */
-// let arr = [2, 7, 11, 15]
+let nums = [2, 7, 11, 15]
 
-// function toSum (arr, target) {
+function twoSum (nums, target) {
+  let map = new Map() //使用map集合
+  for (let i = 0, len = nums.length; i < len; i++) {
+    let a = target - nums[i]
+    if (map.has(a)) {
+      return [map.get(a), i]
+    } else {
+      map.set(nums[i], i)
+    }
+  }
+  return []
 
-//   arr.filter(item => {
-//     return target - item
-//   })
-
-
-// }
+}
+console.log(twoSum(nums, 9));
